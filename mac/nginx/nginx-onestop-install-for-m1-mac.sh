0c6dae1a102dc5d48d59be2c3383f971389a1c7f
spacepro <spacepro@gmail.com>
2025-03-30 17:56:22 +0900

    Update nginx-onestop-install-for-m1-mac.sh

--- mac/nginx/nginx-onestop-install-for-m1-mac.sh
+++ mac/nginx/nginx-onestop-install-for-m1-mac.sh
@@ -1,23 +1,44 @@
-NGINX_VERSION=1.20.2
 INSTALL_PATH=/Users/user/apps/nginx
-ZLIB=1.2.11
-PCRE=8.45
-SSL=openssl-1.1.1l
+ZLIB=1.3.1
+PCRE2=10.45
+SSL=3.4.1
+NGINX=1.26.3
+
+# initialize
 cd ~;mkdir temp_directory;cd temp_directory
-wget https://jaist.dl.sourceforge.net/project/libpng/zlib/$ZLIB/zlib-$ZLIB.tar.gz
+
+# ZLIB
+echo '============= ZLIB (start) ============'
+wget https://github.com/madler/zlib/releases/download/v$ZLIB/zlib-$ZLIB.tar.gz
 tar xvfz zlib-$ZLIB.tar.gz
 cd zlib-$ZLIB && ./configure && make && cd ..
-wget https://jaist.dl.sourceforge.net/project/pcre/pcre/$PCRE/pcre-$PCRE.tar.gz
-tar xvfz pcre-$PCRE.tar.gz
-cd pcre-$PCRE && ./configure && make && cd ..
-wget https://www.openssl.org/source/$SSL.tar.gz
-tar xvfz $SSL.tar.gz
-cd $SSL && ./config && make && cd ..
-wget http://nginx.org/download/nginx-$NGINX_VERSION.tar.gz
-tar xvfz nginx-$NGINX_VERSION.tar.gz
-cd nginx-$NGINX_VERSION
-./configure --with-http_realip_module --with-zlib=../zlib-$ZLIB --with-pcre=../pcre-$PCRE --with-openssl=../$SSL --with-http_ssl_module --with-http_v2_module --with-debug --prefix=$INSTALL_PATH
+echo '============= ZLIB (done) ============'
+
+# PCRE
+echo '============= PCRE (start) ============'
+wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.45/pcre2-$PCRE2.tar.gz
+tar xvfz pcre2-$PCRE2.tar.gz
+cd pcre2-$PCRE2 && ./configure && make && cd ..
+echo '============= PCRE (done) ============'
+
+# SSL
+echo '============= SSL (start) ============'
+wget https://github.com/openssl/openssl/releases/download/openssl-$SSL/openssl-$SSL.tar.gz
+tar xvfz openssl-$SSL.tar.gz
+cd openssl-$SSL && ./config && make && cd ..
+echo '============= SSL (done) ============'
+
+# NGINX
+echo '============= NGINX (start) ============'
+wget http://nginx.org/download/nginx-$NGINX.tar.gz
+tar xvfz nginx-$NGINX.tar.gz
+cd nginx-$NGINX
+./configure --with-http_realip_module --with-zlib=../zlib-$ZLIB --with-pcre=../pcre2-$PCRE2 --with-openssl=../openssl-$SSL --with-http_ssl_module --with-http_v2_module --with-debug --prefix=$INSTALL_PATH
 make
 make install
+echo '============= NGINX (done) ============'
+
+# clean up
 cd ../.. && rm -rf temp_directory
+
 echo "Installed!"
